c5cb64fb5a54f0335cc0b30031e8aed99cbd2e8a
Pradheon <pradheon@gmail.com>
2022-02-02 14:25:23 -0800

    Initial Commit

---
+++ JoshanRai-GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoshanRai-GuessTheFlag
+//
+//  Created by Joshan Rai on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshanRai-GuessTheFlag/JoshanRai_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_GuessTheFlagApp.swift
+//  JoshanRai-GuessTheFlag
+//
+//  Created by Joshan Rai on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
