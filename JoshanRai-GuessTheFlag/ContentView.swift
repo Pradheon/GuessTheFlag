8027c941583f4b05fc0baed8797e23750076cae1
Joshan Rai <pradheon@gmail.com>
2022-04-18 14:39:44 -0700

    v1.9
    - Accessibility

--- JoshanRai-GuessTheFlag/ContentView.swift
+++ JoshanRai-GuessTheFlag/ContentView.swift
@@ -24,7 +24,22 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var nQuestionsAsked = 0
+    
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State var correctAnswer = Int.random(in: 0...2)
     @State private var animationOpacity = 1.0
     @State private var animationCount = 0.0
@@ -68,6 +83,7 @@ struct ContentView: View {
                         .rotation3DEffect(number != correctAnswer ? .degrees(animationCount) : .degrees(0), axis: (x: 2, y: 0, z: 0))
                         // Day 34 - Challege 2 - opacity to 25%
                         .opacity(number != correctAnswer ? animationOpacity : 1.0)
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
                 .frame(maxWidth: .infinity)
